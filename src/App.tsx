8b7fdcec34e5a9bc0edc5eea9790044751a01827
Ibrahimzene <131875836+Ibrahimzene@users.noreply.github.com>
2023-11-09 11:14:20 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Header } from "./components/Header";
+import { LoginForm } from "./components/LoginForm";
+
 function App() {
 	return (
 		<>
-			<h1 className="text-3xl mb-3 text-slate-800">Info Site</h1>
-			<p>Welcome to this site.</p>
+			<Header />
+			<LoginForm/>
 		</>
 	);
 }
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,93 +1,24 @@
-import { useRef, useState, useEffect } from "react";
-
-const initialFormData = {
-	login: "",
-	password: "",
-};
-const initialLegend = "Welcome";
-
-const user = {
-	login: "jim",
-	password: "111",
-};
-
 export const LoginForm = () => {
-	const [formData, setFormData] = useState(initialFormData);
-	const [legend, setLegend] = useState(initialLegend);
-	const [legendColor, setLegendColor] = useState("black");
-	const [errorMessage, setErrorMessage] = useState("");
-	const loginElementRef = useRef(null);
-
-	useEffect(() => {
-		let _legendColor = "";
-		if (legend === initialLegend) {
-			_legendColor = "black";
-		} else {
-			_legendColor = "red";
-		}
-		if (errorMessage === "") {
-			setLegend(initialLegend);
-		} else {
-			setLegend(errorMessage);
-		}
-		setLegendColor(_legendColor);
-	}, [errorMessage, legend]);
-
-	const handleFieldLogin = (value: string) => {
-		const _formData = structuredClone(formData);
-		_formData.login = value;
-		setErrorMessage("");
-		setFormData(_formData);
-	};
-
-	const handleFieldPassword = (value: string) => {
-		const _formData = structuredClone(formData);
-		_formData.password = value;
-		setErrorMessage("");
-		setFormData(_formData);
-	};
-
-	const handleFormSubmit = () => {
-		if (formData.login != user.login) {
-			setErrorMessage("Login is not correct");
-			if (loginElementRef.current) {
-				(loginElementRef.current as HTMLInputElement).focus();
-			}
-		}
-	};
-
 	return (
-		<fieldset className="border border-gray-500 p-4 w-full rounded">
-			<legend style={{ color: legendColor }}>{legend}</legend>
+		<fieldset className="border border-gray-500 p-4 w-full rounded bg-slate-400/50">
+			<legend className="font-bold">Welcome</legend>
 
 			<div className="mb-4 flex gap-2">
 				<label className="w-[5rem]" htmlFor="login">
 					Login:
 				</label>
-				<input
-					type="text"
-					autoFocus
-					ref={loginElementRef}
-					value={formData.login}
-					onChange={(e) => handleFieldLogin(e.target.value)}
-					id="login"
-				/>
+				<input type="text" id="login" />
 			</div>
 
 			<div className="mb-4 flex gap-2">
 				<label className="w-[5rem]" htmlFor="password">
 					Password:
 				</label>
-				<input
-					type="password"
-					value={formData.password}
-					onChange={(e) => handleFieldPassword(e.target.value)}
-					id="password"
-				/>
+				<input type="password" id="password" />
 			</div>
 
 			<div className="mt-5 flex justify-end pr-3">
-				<button onClick={handleFormSubmit}>Time to run!</button>
+				<button>Time to run!</button>
 			</div>
 		</fieldset>
 	);
